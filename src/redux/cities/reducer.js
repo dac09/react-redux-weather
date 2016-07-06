b29538cc143c8da46e02f97aebec8214f2c8c692
Daniel Choudhury <dannychoudhury@gmail.com>
2016-07-06 23:14:49 +0100

    Update city

--- src/redux/cities/reducer.js
+++ src/redux/cities/reducer.js
@@ -6,8 +6,11 @@ export const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case cityActions.ADD_CITY:
-      //@TODO: Validate if city is valid
+      // @TODO: Validate if city is valid
       return Object.assign({}, state, {[action.city]: {}})
+
+    case cityActions.UPDATE_CITY:
+      return Object.assign({}, state, {[action.city]: action.data})
 
     case cityActions.REMOVE_CITY:
     // Use reflect so that no try catch block
